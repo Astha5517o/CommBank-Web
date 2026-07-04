31ef4fdb2d07f4d95039fa8b4835504c759b145a
Astha Visuals <astha5517o@gmail.com>
2026-07-04 18:52:47 +0530

    Update types.ts

--- src/api/types.ts
+++ src/api/types.ts
@@ -27,42 +27,10 @@ export interface Goal {
   accountId: string
   transactionIds: string[]
   tagIds: string[]
+  icon?: string;
 }
 
 export interface Tag {
   id: string
   name: string
 }
-
-export interface Transaction {
-  id: string
-  transactionType: 'Debit' | 'Credit' | 'Transfer'
-  amount: number
-  dateTime: Date
-  goalId?: string
-  description: string
-  tagIds: string[]
-}
-
-export interface User {
-  id: string
-  name: string
-  email: string
-  applicationIds: string[]
-}
-
-export enum AccountType {
-  GoalSaver,
-  NetBankSaver,
-}
-
-export enum ApplicationStatus {
-  Received,
-  Assigned,
-  UnderReview,
-  Approved,
-  Rejected,
-}
-
-export type ModalContent = Goal
-export type ModalType = 'Goal'
