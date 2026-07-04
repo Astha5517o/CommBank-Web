c179639a05048980be86d5bc710e17e3eb1ba0c1
Astha Visuals <astha5517o@gmail.com>
2026-07-04 19:58:17 +0530

    Refactor emoji selection handler in GoalManager

--- src/ui/features/goalmanager/GoalManager.tsx
+++ src/ui/features/goalmanager/GoalManager.tsx
@@ -31,7 +31,7 @@ export default function GoalManager(props: Props) {
     )
   }
 
-  const handleEmojiSelect = (emoji: any) => {
+  const pickEmojiOnClick = (emoji: any) => {
     dispatch(updateGoal({ ...props.goal!, icon: emoji.native }))
     setShowPicker(false)
   }
@@ -54,7 +54,99 @@ export default function GoalManager(props: Props) {
 
       {showPicker && (
         <PickerContainer>
-          <Picker onSelect={handleEmojiSelect} title="Pick a goal icon" emoji="point_up" />
+          <Picker onSelect={pickEmojiOnClick} title="Pick a goal icon" emoji="point_up" />
+        </PickerContainer>
+      )}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+`
+
+const HeaderRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 2rem;
+`
+
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 2rem;
+`
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin: 0;
+`
+
+const PickerContainer = styled.div`
+  position: absolute;
+  z-index: 10;
+  margin-top: 7rem;
+`import 'date-fns'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Goal } from '../../../api/types'
+import { updateGoal } from '../../../store/goals/actions'
+import { useAppDispatch } from '../../../store/hooks'
+import LoadingPanel from '../../components/LoadingPanel'
+import AddIconButton from './AddIconButton'
+import GoalIcon from './GoalIcon'
+import { Picker } from 'emoji-mart'
+import 'emoji-mart/css/emoji-mart.css'
+
+type Props = {
+  goal: Goal | null
+  isLoading: boolean
+}
+
+export default function GoalManager(props: Props) {
+  const dispatch = useAppDispatch()
+  const [showPicker, setShowPicker] = useState(false)
+
+  if (props.isLoading) {
+    return <LoadingPanel />
+  }
+
+  if (!props.goal) {
+    return (
+      <Container>
+        <Title>No Goal Selected</Title>
+      </Container>
+    )
+  }
+
+  const pickEmojiOnClick = (emoji: any) => {
+    dispatch(updateGoal({ ...props.goal!, icon: emoji.native }))
+    setShowPicker(false)
+  }
+
+  return (
+    <Container>
+      <HeaderRow>
+        <GoalIcon 
+          icon={props.goal.icon || null} 
+          onClick={() => setShowPicker(!showPicker)} 
+        />
+        <TitleContainer>
+          <Title>{props.goal.name}</Title>
+          <AddIconButton 
+            hasIcon={!!props.goal.icon} 
+            onClick={() => setShowPicker(!showPicker)} 
+          />
+        </TitleContainer>
+      </HeaderRow>
+
+      {showPicker && (
+        <PickerContainer>
+          <Picker onSelect={pickEmojiOnClick} title="Pick a goal icon" emoji="point_up" />
         </PickerContainer>
       )}
     </Container>
